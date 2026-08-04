deb061bc850f2a4f480d6093e0210c5669b283fe
JOHN NIL <jnilbd@gmail.com>
2026-08-04 16:23:33 +0600

    feat(core): complete plugin bootstrap

--- includes/class-plugin.php
+++ includes/class-plugin.php
@@ -2,25 +2,41 @@
 
 namespace Bazario\SmartOrder;
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
-}
+defined( 'ABSPATH' ) || exit;
 
 class Plugin {
 
-	public static function init() {
-		$plugin = new self();
-		$plugin->load();
-	}
+    /**
+     * Initialize plugin.
+     */
+    public static function init() {
+        $plugin = new self();
+        $plugin->load_dependencies();
+        $plugin->run();
+    }
 
-	private function load() {
+    /**
+     * Load required classes.
+     */
+    private function load_dependencies() {
 
-		require_once BSO_PLUGIN_PATH . 'includes/class-loader.php';
-		require_once BSO_PLUGIN_PATH . 'admin/class-admin.php';
-		require_once BSO_PLUGIN_PATH . 'public/class-public.php';
+        require_once BSO_PLUGIN_PATH . 'includes/class-loader.php';
+        require_once BSO_PLUGIN_PATH . 'admin/class-admin.php';
+        require_once BSO_PLUGIN_PATH . 'public/class-public.php';
 
-		new Loader();
-		new Admin();
-		new Public_Class();
-	}
+    }
+
+    /**
+     * Run plugin.
+     */
+    private function run() {
+
+        new Loader();
+
+        if ( is_admin() ) {
+            new Admin();
+        }
+
+        new Public_Class();
+    }
 }
