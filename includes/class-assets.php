37ef59863408191bfded2abd443c4161fb6737ec
JOHN NIL <jnilbd@gmail.com>
2026-08-04 16:29:46 +0600

    feat(ui): add professional button design

--- includes/class-assets.php
+++ includes/class-assets.php
@@ -8,14 +8,13 @@ class Assets {
 
     public function __construct() {
 
-        add_action( 'wp_enqueue_scripts', [ $this, 'frontend_assets' ] );
-        add_action( 'admin_enqueue_scripts', [ $this, 'admin_assets' ] );
+        add_action(
+            'wp_enqueue_scripts',
+            [ $this, 'frontend_assets' ]
+        );
 
     }
 
-    /**
-     * Frontend Assets
-     */
     public function frontend_assets() {
 
         wp_enqueue_style(
@@ -23,36 +22,6 @@ class Assets {
             BSO_PLUGIN_URL . 'assets/css/frontend.css',
             [],
             BSO_VERSION
-        );
-
-        wp_enqueue_script(
-            'bso-frontend',
-            BSO_PLUGIN_URL . 'assets/js/frontend.js',
-            [ 'jquery' ],
-            BSO_VERSION,
-            true
-        );
-
-    }
-
-    /**
-     * Admin Assets
-     */
-    public function admin_assets() {
-
-        wp_enqueue_style(
-            'bso-admin',
-            BSO_PLUGIN_URL . 'assets/css/admin.css',
-            [],
-            BSO_VERSION
-        );
-
-        wp_enqueue_script(
-            'bso-admin',
-            BSO_PLUGIN_URL . 'assets/js/admin.js',
-            [ 'jquery' ],
-            BSO_VERSION,
-            true
         );
 
     }
