7feb463f0e3ac2e874945b6f8db7fd47d529d61e
JOHN NIL <jnilbd@gmail.com>
2026-08-04 16:49:07 +0600

    refactor(core): improve plugin bootstrap

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'jnilbd/bazario-smart-order',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '735e51ab68406f63a82de445d2c2760e60d90706',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'jnilbd/bazario-smart-order' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '735e51ab68406f63a82de445d2c2760e60d90706',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
