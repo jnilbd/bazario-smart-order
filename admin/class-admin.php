7ceacc94bf2ee7c1fa10bcc6dd6aa21e45565717
JOHN NIL <jnilbd@gmail.com>
2026-08-04 16:26:35 +0600

    feat(admin): add settings page

--- admin/class-admin.php
+++ admin/class-admin.php
@@ -7,10 +7,47 @@ defined( 'ABSPATH' ) || exit;
 class Admin {
 
     public function __construct() {
-        add_action( 'admin_menu', [ $this, 'menu' ] );
+
+        add_action( 'admin_menu', [ $this, 'register_menu' ] );
+
     }
 
-    public function menu() {
-        // Admin menu.
+    /**
+     * Register Admin Menu
+     */
+    public function register_menu() {
+
+        add_menu_page(
+            'Bazario Smart Order',
+            'Smart Order',
+            'manage_options',
+            'bazario-smart-order',
+            [ $this, 'settings_page' ],
+            'dashicons-cart',
+            56
+        );
+
     }
+
+    /**
+     * Settings Page
+     */
+    public function settings_page() {
+        ?>
+        <div class="wrap">
+            <h1>Bazario Smart Order</h1>
+
+            <form method="post" action="options.php">
+
+                <?php
+                settings_fields( 'bso_settings_group' );
+                do_settings_sections( 'bazario-smart-order' );
+                submit_button( 'Save Settings' );
+                ?>
+
+            </form>
+        </div>
+        <?php
+    }
+
 }
